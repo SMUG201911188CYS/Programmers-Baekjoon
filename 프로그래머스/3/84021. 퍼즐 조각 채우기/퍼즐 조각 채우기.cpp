af9ebff64b9a8ac0fd94c5302f42f37f22869b60
최용성 <53203389+SMUG201911188CYS@users.noreply.github.com>
2024-10-31 03:33:06 +0900

    Update 퍼즐 조각 채우기.cpp

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
@@ -18,8 +18,8 @@ struct Pos // 위치
 string board; // 보드 빈공간
 string puzzle; // 퍼즐
 
-unordered_map<int, vector<Pos>> puzzle_pos; // 퍼즐들의 위치
-unordered_map<int, vector<Pos>> board_pos;
+unordered_map<int, vector<Pos>> puzzle_pos; // 퍼즐의 위치
+unordered_map<int, vector<Pos>> board_pos; // 보드의 위치
 
 int board_size; // 크기
 
@@ -52,11 +52,11 @@ void bfs(vector<vector<int>> & v, int condition, vector<vector<bool>> & is_visit
         if(is_visited[current_x][current_y]) // 이미 방문했다면, continue;
             continue;
         
-        is_visited[current_x][current_y] = true;
+        is_visited[current_x][current_y] = true; // 방문 처리
         
         s += current_c; // 방향 char 저장
         
-        pos_q.push(Pos(current_x, current_y));
+        pos_q.push(Pos(current_x, current_y)); // 위치 저장
         
         for(int i = 0; i < 4; i++)
         {
@@ -70,8 +70,6 @@ void bfs(vector<vector<int>> & v, int condition, vector<vector<bool>> & is_visit
     
     if(condition == 1) // 퍼즐
     {
-        //cout << "puzzle : " << s << '\n';
-        
         while(!pos_q.empty()) // 퍼즐 위치 저장
         {
             Pos temp = pos_q.front();
@@ -83,9 +81,7 @@ void bfs(vector<vector<int>> & v, int condition, vector<vector<bool>> & is_visit
     }
     else // 보드
     {
-        //cout << "board : " << s << '\n';
-
-        while(!pos_q.empty()) // 퍼즐 위치 저장
+        while(!pos_q.empty()) // 보드 위치 저장
         {
             Pos temp = pos_q.front();
             board_pos[0].push_back(temp);
@@ -116,45 +112,16 @@ int solution(vector<vector<int>> game_board, vector<vector<int>> table)
     
     board_size = table.size();
 
-    /*
-    for(auto x : game_board)
-    {
-        for(auto y : x)
-        {
-            if(y == 1)
-                cout << setw(2) << "⬛️ ";
-            else
-                cout << setw(2) << "⬜️ ";
-        }
-        cout << '\n';
-    }
-     
-    cout << '\n';
-    */
     
     for(int i = 0; i  < 4; i++)
     {
-        vector<vector<bool>> table_visited(board_size, vector<bool>(board_size, false));
-        /*for(auto x : table)
-        {
-            for(auto y : x)
-            {
-                if(y == 1)
-                    cout << setw(2) << "⬛️ ";
-                else
-                    cout << setw(2) << "⬜️ ";
-            }
-            cout << '\n';
-        }
-        cout << '\n';
-        */
         vector<vector<bool>> board_visited(board_size, vector<bool>(board_size, false)); // game_board 방문 체크
     
         for(int board_i = 0; board_i < board_size; board_i++) // 보드 빈 공간 찾기 시작
-            {
+        {
             for(int board_j = 0; board_j < board_size; board_j++)
             {
-                if(game_board[board_i][board_j] == 0 && !board_visited[board_i][board_j])
+                if(game_board[board_i][board_j] == 0 && !board_visited[board_i][board_j]) // 빈 공간 이거나 방문하지 않았다면,
                 {
                     board = "";
                     board_pos.clear();
@@ -164,7 +131,7 @@ int solution(vector<vector<int>> game_board, vector<vector<int>> table)
                     bool is_correct = false;
                 
                     for(int n = 0; n <= 4; n++) // 맞는 퍼즐 찾기 시작, table 90도, 180도, 270도 회전
-                        {
+                    {
                         vector<vector<bool>> table_visited(board_size, vector<bool>(board_size, false));
 
                         for(int i = 0; i < board_size && !is_correct; i++)
@@ -180,25 +147,20 @@ int solution(vector<vector<int>> game_board, vector<vector<int>> table)
                                     
                                     if(board == puzzle) // 퍼즐과 빈 공간이 같다면
                                     {
-                                        int distance_x = board_pos[0][0].x - puzzle_pos[0][0].x;
-                                        int distance_y = board_pos[0][0].y - puzzle_pos[0][0].y;
+                                        int distance_x = board_pos[0][0].x - puzzle_pos[0][0].x; // 빈 공간과 퍼즐의 x값 좌표 차이
+                                        int distance_y = board_pos[0][0].y - puzzle_pos[0][0].y; // 빈 공간과 퍼즐의 y값 좌표 차이
                                         bool flag = true;
 
-                                        //cout << "distance : " << distance_x << ", " << distance_y << '\n';
                                         for(int k = 0; k < puzzle_pos[0].size(); k++)
                                         {
-                                            //cout << "current : " << board_pos[0][k].x - puzzle_pos[0][k].x << ", " << board_pos[0][k].y - puzzle_pos[0][k].y << '\n';
-                                            // << "current : " << board_pos[0][k].x << ", " << board_pos[0][k].y << ", " << puzzle_pos[0][k].x << ", " << puzzle_pos[0][k].y << '\n';
                                             if(board_pos[0][k].x - puzzle_pos[0][k].x != distance_x || board_pos[0][k].y - puzzle_pos[0][k].y != distance_y)
                                             {
-                                                flag = false;
+                                                flag = false; // 좌표 값의 차이가 다르다면, 이는 같은 모양이 아님
                                             }
                                         }
 
-                                        if(flag)
+                                        if(flag) // 같은 모양이라면,
                                         {
-                                            //cout << "correct puzzle : " << puzzle << '\n';
-
                                             answer += puzzle.length();
 
                                             for(auto pos : puzzle_pos[0]) // 찾은 퍼즐 0으로 초기화
@@ -212,15 +174,11 @@ int solution(vector<vector<int>> game_board, vector<vector<int>> table)
                                 }
                             }
                         }
-                        //cout << '\n';
-                    
                         rotate_v(table); // 회전
-                        }
+                    }
                 }
             }
-            }
-
-        //cout << answer << '\n';
+        }
         return answer;
     }
 }
